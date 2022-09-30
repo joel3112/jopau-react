22d5eea528786fcf4c666890c6f53de9aa73e394
Joel Paucarima Franco <jopau3112@gmail.com>
2022-09-30 19:32:05 +0200

    fix: remove console log 1

--- packages/utils/src/breakpoint/breakpoint.ts
+++ packages/utils/src/breakpoint/breakpoint.ts
@@ -65,7 +65,6 @@ export class BreakpointsHelper {
   }
 
   up(min: Breakpoint): boolean {
-    console.log('up');
     return this.#getValue(min) !== undefined ? this.#targetWidth >= this.#getValue(min) : false;
   }
 }
