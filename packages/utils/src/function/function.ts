5b3e1723f27091d0499216d099439937d2b0896e
Joel Paucarima Franco <joel.dev.3112@outlook.es>
2023-01-03 13:12:32 +0100

    feat: fix import 2

--- packages/utils/src/function/function.ts
+++ packages/utils/src/function/function.ts
@@ -1,9 +1,9 @@
 import _ from 'lodash';
-import { TAny, TArray } from '../index';
+import { TAny } from '../index';
 
 export type TFunction = (...args: never[]) => TAny;
 export type TFunctionVoid = (...args: never[]) => void;
 
-export const range = (start: number, end?: number, step?: number): TArray<number> => {
+export const range = (start: number, end?: number, step?: number): Array<number> => {
   return _.range(start, end, step);
 };
