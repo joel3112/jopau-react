e96c0253f39e0c1b89e12a8b4a7e9554b660a1ad
Joel Paucarima Franco <joel.dev.3112@outlook.es>
2023-01-03 14:18:02 +0100

    feat: typeversions

--- packages/utils/src/function/function.ts
+++ packages/utils/src/function/function.ts
@@ -5,8 +5,13 @@ export type TFunction = (...args: never[]) => TAny;
 export type TFunctionVoid = (...args: never[]) => void;
 
 export const range = _.range;
+
 export const debounce = _.debounce;
+
 export const delay = _.delay;
+
 export const noop = _.noop;
+
 export const random = _.random;
+
 export const throttle = _.throttle;
